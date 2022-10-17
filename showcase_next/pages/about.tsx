272a861747af332d893b79205bc6e8120def9fa9
heraldjose10 <heraldjose10@gmail.com>
2022-10-18 00:18:28 +0100

    update about, contact page fonts

--- showcase_next/pages/about.tsx
+++ showcase_next/pages/about.tsx
@@ -5,16 +5,16 @@ import Layout from '../components/Layout'
 const AboutPage = () => (
   <Layout title="About Me">
     <motion.div
-      className='w-[1100px] grow-0 mx-auto mt-[100px] px-10'
+      className='max-w-screen-lg grow-0 mx-auto mt-28 px-10'
       initial={{ opacity: 0, y: '100%' }}
       animate={{ opacity: 1, y: 0, transition: { duration: .4 } }}
       exit={{ opacity: 0, y: '100%' }}
     >
-      <h1 className='text-5xl font-extrabold md:text-7xl lg:text-8xl'>Herald<br /> Olakkengil</h1>
-      <p className='text-lg my-8 lg:text-2xl'>
+      <h1 className='text-5xl font-extrabold md:text-7xl lg:text-8xl font-Fjalla'>Herald<br /> Olakkengil</h1>
+      <p className='text-lg my-8 lg:text-2xl font-Noto'>
         I'm a web developer learning <a href='https://flask.palletsprojects.com/en/2.1.x/' className='underline' target='_blank'>Flask</a>, and <a href='https://reactjs.org/' className='underline' target='_blank'>React</a> frameworks. This is my portfolio website. I made this webpage using <a href='https://nextjs.org/' className='underline' target='_blank'>NextJS</a>. I started using React back in 2021. Check out my <a href='https://github.com/heraldjose10' className='underline' target='_blank'>Github</a> for more projects. Follow me on <a href='https://twitter.com/heraldjose10' className='underline' target='_blank'>Twitter</a> and <a href='https://www.linkedin.com/in/herald-olakkengil-5b47491b3/' className='underline' target='_blank'>LinkedIn</a> to connect with me.
       </p>
-      <p className='text-lg my-8 lg:text-2xl'>
+      <p className='text-lg my-8 lg:text-2xl font-Noto'>
         Feel free to write your feedback to me!
       </p>
     </motion.div>
--- showcase_next/pages/contact.tsx
+++ showcase_next/pages/contact.tsx
@@ -5,7 +5,7 @@ import Layout from "../components/Layout";
 const ContactPage = () => (
     <Layout>
         <motion.div
-            className='w-[1100px] grow-0 mx-auto mt-[100px] px-10 flex flex-col gap-20'
+            className='max-w-screen-lg grow-0 mx-auto mt-28 px-10 flex flex-col gap-20'
             initial={{ opacity: 0, y: '100%' }}
             animate={{ opacity: 1, y: 0, transition: { duration: .2 } }}
             exit={{ opacity: 0, y: '100%', transition: { duration: .2 } }}
@@ -14,10 +14,9 @@ const ContactPage = () => (
             {/* address section */}
             <section className="flex flex-col gap-6">
                 <div className="flex items-end gap-4 w-fit">
-                    {/* <h2 className="text-5xl md:text-8xl font-semibold bg-green-500 text-white animate-grand-reveal">Address</h2> */}
-                    <h1 className="text-5xl font-extrabold md:text-7xl lg:text-8xl">Address</h1>
+                    <h1 className="text-5xl font-extrabold md:text-7xl lg:text-8xl font-Fjalla">Address</h1>
                 </div>
-                <address className="text-lg md:text-xl">
+                <address className="text-lg md:text-xl font-Noto">
                     Olakkengil House<br></br>
                     Pavaratty P.O<br></br>
                     Thrissur<br></br>
@@ -28,11 +27,10 @@ const ContactPage = () => (
             {/* email section */}
             <section className="flex flex-col gap-6">
                 <div className="flex items-end gap-4 w-fit">
-                    <h1 className="text-5xl font-extrabold md:text-7xl lg:text-8xl">Email</h1>
-                    {/* <h2 className="text-5xl md:text-8xl font-semibold bg-green-500 text-white animate-grand-reveal">Email</h2> */}
+                    <h1 className="text-5xl font-extrabold md:text-7xl lg:text-8xl font-Fjalla">Email</h1>
                 </div>
                 <a
-                    className="text-lg md:text-xl"
+                    className="text-lg md:text-xl font-Noto"
                     href="mailto:heraldjose10@gmail.com"
                 >
                     heraldjose10@gmail.com
